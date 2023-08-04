d3e0ba981b5f0cd76b5d08f4b0cc222dc2bbfa37
LinYang-AI <linyoung1214@outlook.com>
2023-08-05 00:07:20 +0200

    update main.py for trip analysis

--- src/jobs/trip_analysis/main.py
+++ src/jobs/trip_analysis/main.py
@@ -1,18 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, hour, dayofweek, month
 from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression
+
 
 # Create a SparkSession
 spark = SparkSession.builder.appName("TaxiAnalysis").getOrCreate()
 
-def load_data():
+
+def load_data(path):
     """
-    column names: VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, RatecodeID, store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge, airport_fee
+    column names: VendorID, tpep_pickup_datetime, tpep_dropoff_datetime,
+    passenger_count, trip_distance, RatecodeID, store_and_fwd_flag,
+    PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax,
+    tip_amount, tolls_amount, improvement_surcharge, total_amount,
+    congestion_surcharge, airport_fee
     """
     # Load the NYC taxi data from Parquet files
-    data_nyc = spark.read.parquet("./NYC/*.parquet")
-    
+    data_nyc = spark.read.parquet(path)
+
     return data_nyc
+
 
 def trip_analysis(data):
     """_summary_
@@ -20,111 +30,154 @@ def trip_analysis(data):
     and month of year. This can reveal patterns such as longer trips during rush hours, on 
     weekends, or during holiday seasons.
     """
-    
-    
-    
     # Average duration and distance of rides by time of day, day of week, and month of year
-    window = Window.orderBy("pickup_datetime")
+    data = data.withColumn("trip_duration_minutes",
+                           F.unix_timestamp("tpep_dropoff_datetime") - F.unix_timestamp("tpep_pickup_datetime"))
 
-    trip_analysis = data.withColumn("hour", hour("pickup_datetime")) \
-                       .withColumn("day_of_week", dayofweek("pickup_datetime")) \
-                       .withColumn("month", month("pickup_datetime"))
+    data = data.withColumn("avg_distance", (F.col(
+        "trip_distance") / F.col("passenger_count")))
+    data = data.withColumn("pickup_hour", F.hour("tpep_pickup_datetime"))
+    data = data.withColumn("pickup_day_of_week",
+                           F.dayofweek("tpep_pickup_datetime"))
+    data = data.withColumn("pickup_month", F.month("tpep_pickup_datetime"))
 
-    avg_duration_by_hour = trip_analysis.groupBy("hour").avg("trip_duration_minutes")
-    avg_distance_by_hour = trip_analysis.groupBy("hour").avg("trip_distance")
+    agg_df = data.groupBy("pickup_hour", "pickup_day_of_week", "pickup_month").agg(F.avg("trip_duration").alias(
+        "avg_duration"), F.avg("avg_distance").alias("avg_distance")).orderBy("pickup_hour", "pickup_day_of_week", "pickup_month")
 
-    avg_duration_by_day = trip_analysis.groupBy("day_of_week").avg("trip_duration_minutes")
-    avg_distance_by_day = trip_analysis.groupBy("day_of_week").avg("trip_distance")
+    pickup_locations = data.groupBy(
+        "PULocationID").count().orderBy(F.desc("count"))
+    top_pickup_locations = pickup_locations.limit(10)
 
-    avg_duration_by_month = trip_analysis.groupBy("month").avg("trip_duration_minutes")
-    avg_distance_by_month = trip_analysis.groupBy("month").avg("trip_distance")
+    dropoff_locations = data.groupBy(
+        "DOLocationID").count().orderBy(F.desc("count"))
+    top_dropoff_locations = dropoff_locations.limit(10)
 
-    avg_duration_by_hour.show()
-    avg_distance_by_hour.show()
-    avg_duration_by_day.show()
-    avg_distance_by_day.show()
-    avg_duration_by_month.show()
-    avg_distance_by_month.show()
+    return agg_df, top_pickup_locations, top_dropoff_locations
 
-def popular_locations(data):
-    # Identify the top 10 pickup and dropoff locations
-    top_pickup_locations = data.groupBy("PULocationID").count().orderBy(col("count").desc()).limit(10)
-    top_dropoff_locations = data.groupBy("DOLocationID").count().orderBy(col("count").desc()).limit(10)
-
-    top_pickup_locations.show()
-    top_dropoff_locations.show()
 
 def tip_analysis(data):
-    # Tip percentage by trip
-    data = data.withColumn("tip_percentage", col("tip_amount") / col("total_amount") * 100)
 
-    # Tips by time: Does the time of day, week, or year affect tipping behavior?
-    tip_by_hour = data.groupBy(hour("pickup_datetime")).avg("tip_percentage").orderBy("hour")
-    tip_by_day_of_week = data.groupBy(dayofweek("pickup_datetime")).avg("tip_percentage").orderBy("dayofweek")
-    tip_by_month = data.groupBy(month("pickup_datetime")).avg("tip_percentage").orderBy("month")
+    tip_analysis_df = data.withColumn("tip_percentage", F.col(
+        "tip_amount") / F.col("total_amount") * 100)
+    tip_analysis_df = tip_analysis_df.withColumn("tip_percentage", F.when(F.col(
+        "tip_percentage") <= 100, F.col("tip_percentage")).otherwise(0))  # Handle possible outliers
 
-    tip_by_hour.show()
-    tip_by_day_of_week.show()
-    tip_by_month.show()
+    # Group by pickup and dropoff locations and calculate average tip percentage and average distance
+    tip_location_analysis = tip_analysis_df.groupBy(
+        "PULocationID",
+        "DOLocationID"
+    ).agg(
+        F.avg("tip_percentage").alias("avg_tip_percentage"),
+        F.avg("trip_distance").alias("avg_distance")
+    ).orderBy(F.desc("avg_tip_percentage"))
+
+    tip_time_analysis = tip_analysis_df.groupBy(
+        "pickup_hour",
+        "pickup_day_of_week",
+        "pickup_month"
+    ).agg(
+        F.avg("tip_percentage").alias("avg_tip_percentage"),
+        F.sum("tip_amount").alias("total_tip_amount")
+    ).orderBy(
+        "pickup_hour",
+        "pickup_day_of_week",
+        "pickup_month")
+
+    payment_tip_analysis = tip_analysis_df.groupBy("payment_type").agg(F.avg("tip_percentage").alias("avg_tip_percentage"),
+                                                                       F.avg("tip_amount").alias(
+                                                                           "avg_tip_amount"),
+                                                                       F.sum("tip_amount").alias("total_tip_amount"))
+
+    return tip_location_analysis, tip_time_analysis, payment_tip_analysis
+
 
 def fare_analysis(data):
-    # Can you calculate the average fare by pickup & drop-off location?
-    avg_fare_by_pickup_dropoff = data.groupBy("PULocationID", "DOLocationID").avg("fare_amount")
-    avg_fare_by_pickup_dropoff.show()
+    fare_location_analysis = data.groupBy("PULocationID", "DOLocationID").\
+        agg(F.avg("fare_amount").alias("avg_fare")).\
+        orderBy(F.desc("avg_fare"))
 
-    # Can you calculate the average fare by Passenger count?
-    avg_fare_by_passenger_count = data.groupBy("passenger_count").avg("fare_amount")
-    avg_fare_by_passenger_count.show()
+    fare_passenger_analysis = data.groupBy("passenger_count").\
+        agg(F.avg("fare_amount").alias("avg_fare")).\
+        orderBy("passenger_count")
+    fare_distance_correlation = data.select(F.corr("fare_amount", "trip_distance").
+                                            alias("correlation")).\
+        collect()[0]["correlation"]
 
-    # Can you correlate the fare amount and the distance trip?
-    data = data.withColumn("fare_per_distance", col("fare_amount") / col("trip_distance"))
+    return fare_location_analysis, fare_passenger_analysis, fare_distance_correlation
 
-    correlation_fare_distance = data.stat.corr("fare_per_distance", "trip_distance")
-    print("Correlation between fare and trip distance:", correlation_fare_distance)
 
 def traffic_analysis(data):
-    # Calculate the average speed of a trip (average trip speed in miles per hour)
-    data = data.withColumn("trip_speed", col("trip_distance") / (col("trip_duration_minutes") / 60))
+    trip_speed_df = data.withColumn("trip_speed", (F.col(
+        "trip_distance") / (F.col("trip_duration") / 3600)))
 
-    # Group the average speed by trip then hour, day, week
-    avg_speed_by_hour = data.groupBy(hour("pickup_datetime")).avg("trip_speed").orderBy("hour")
-    avg_speed_by_day_of_week = data.groupBy(dayofweek("pickup_datetime")).avg("trip_speed").orderBy("dayofweek")
-    avg_speed_by_month = data.groupBy(month("pickup_datetime")).avg("trip_speed").orderBy("month")
+    trip_time_speed_analysis = trip_speed_df.groupBy("PULocationID",
+                                                     "DOLocationID",
+                                                     "pickup_hour",
+                                                     "pickup_day_of_week",
+                                                     "pickup_month").\
+        agg(F.avg("trip_speed").
+            alias("avg_trip_speed")).\
+        orderBy("PULocationID",
+                "DOLocationID",
+                "pickup_hour",
+                "pickup_day_of_week",
+                "pickup_month")
+    return trip_time_speed_analysis
 
-    avg_speed_by_hour.show()
-    avg_speed_by_day_of_week.show()
-    avg_speed_by_month.show()
 
 def demand_prediction(data):
-    # Feature engineering: Use the date and time of the pickups to create features for the model
-    data = data.withColumn("hour_of_day", hour("pickup_datetime"))
-    data = data.withColumn("day_of_week", dayofweek("pickup_datetime"))
-    data = data.withColumn("month", month("pickup_datetime"))
 
-    # Regression model: Use linear regression to predict the number of pickups in the next hour
-    from pyspark.ml.feature import VectorAssembler
-    from pyspark.ml.regression import LinearRegression
+    demand_df = data.withColumn("pickup_hour", F.hour("tpep_pickup_datetime"))
+    demand_df = demand_df.withColumn(
+        "pickup_day_of_week", F.dayofweek("tpep_pickup_datetime"))
+    demand_df = demand_df.withColumn(
+        "pickup_month", F.month("tpep_pickup_datetime"))
 
-    assembler = VectorAssembler(inputCols=["hour_of_day", "day_of_week", "month"], outputCol="features")
-    data = assembler.transform(data)
+    feature_columns = ["pickup_hour", "pickup_day_of_week", "pickup_month"]
+    assembler = VectorAssembler(
+        inputCols=feature_columns, outputCol="features")
+    demand_df = assembler.transform(demand_df)
 
-    lr = LinearRegression(featuresCol="features", labelCol="pickups_in_next_hour")
-    lr_model = lr.fit(data)
+    feature_columns = ["pickup_hour", "pickup_day_of_week", "pickup_month"]
+    assembler = VectorAssembler(
+        inputCols=feature_columns, outputCol="input_features")
+    demand_df = assembler.transform(demand_df)
 
-    # Make predictions for the next hour based on the features
-    next_hour_data = spark.createDataFrame([(18, 2, 7)], ["hour_of_day", "day_of_week", "month"])
-    next_hour_data = assembler.transform(next_hour_data)
-    predictions = lr_model.transform(next_hour_data)
+    # Aggregate data for regression
+    regression_df = demand_df.groupBy("pickup_hour").agg(
+        F.sum("passenger_count").alias("total_pickups"),
+        F.first("input_features").alias("features"))
 
-    predicted_pickups = predictions.select("prediction").first()[0]
-    print("Predicted number of pickups in the next hour:", predicted_pickups)
+    lr = LinearRegression(featuresCol="features", labelCol="total_pickups")
+    lr_model = lr.fit(regression_df)
+
+    return lr_model
 
 
 if __name__ == "__main__":
-    data = load_data()
-    trip_analysis(data)
-    popular_locations(data)
-    tip_analysis(data)
-    fare_analysis(data)
-    traffic_analysis(data)
-    demand_prediction(data)
+    path = "./NYC/*.parquet"
+    data = load_data(path)
+    trip = trip_analysis(data)
+    tip = tip_analysis(data)
+    fare = fare_analysis(data)
+    traffic = traffic_analysis(data)
+    demand_model = demand_prediction(data)
+
+    trip[0].show()
+    print("Top 10 Pickup Locations:")
+    trip[1].show()
+    print("Top 10 Dropoff Locations:")
+    trip[2].show()
+    print("Tip Analysis by Location:")
+    tip[0].show()
+    print("Tip Analysis by Time:")
+    tip[1].show()
+    print("Payment and Tip Analysis:")
+    tip[2].show()
+    print("Average Fare by Pickup & Drop Location:")
+    fare[0].show()
+    print("Average Fare by Passenger Count:")
+    fare[1].show()
+    print("Correlation between Fare Amount and Trip Distance:")
+    print("Correlation coefficient:", fare[2])
+    traffic.show()
